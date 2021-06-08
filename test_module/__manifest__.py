b4b3631d0c25b38cfd144bc30217c8cb968ccf4b
Friedrich S <friedrich.seidenspinner@aejgmbh.com>
2021-06-08 14:00:21 +0200

    [ADD] Add new module

---
+++ test_module/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name' : 'Test module',
+    'version' : '1.1',
+    'summary': 'Test',
+    'category': 'Test',
+    'depends' : [],
+    'data': [
+    ],
+    'installable': True,
+    'application': True,
+}
