a17c830230f0f538efa61f4443632f929e68943d
Friedrich S <friedrich.seidenspinner@aejgmbh.com>
2021-06-08 14:23:33 +0200

    [ADD] Add new module school management

---
+++ school_management/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name' : 'School Management',
+    'version' : '1.1',
+    'summary': 'Test',
+    'category': 'Test',
+    'depends' : ['base'],
+    'data': ['security/ir.model.access.csv',
+             'views/student_viwes.xml'
+    ],
+    'installable': True,
+    'application': True,
+}
