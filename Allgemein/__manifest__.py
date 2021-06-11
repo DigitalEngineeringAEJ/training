9dc77be0a970f899ec66e2e226dd0b6b6d1ae73f
AEJ-Member <friedrich.seidenspinner@aejgmbh.com>
2021-06-11 14:55:20 +0200

    [ADD] new Modul

---
+++ Allgemein/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name' : 'Allgemein',
+    'version' : '0.1',
+    'summary': 'Allgemeine',
+    'description': """
+    Hier haben wir eine allgemeine Beschreibung.
+    """,
+    'category': 'Test',
+    'depends' : ['base'],
+    'data': [
+
+    ],
+    'demo': [
+
+    ],
+
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
