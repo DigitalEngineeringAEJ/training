f0d0ed0362df6521c08d9f80d9715f0e5c5e5b76
AEJ-Member <friedrich.seidenspinner@aejgmbh.com>
2021-06-11 11:21:03 +0200

    [ADD] NEW Module FSMOD

---
+++ FSMOD/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name' : 'FSMOD',
+    'version' : '0.1',
+    'summary': 'FSMOD',
+    'description': """
+    FSMOD is for testing
+    """,
+    'category': 'test',
+    'website': 'https://www.eaj.odoo.com',
+    'depends' : ['base'],
+    'data': [
+
+    ],
+
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
