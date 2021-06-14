3b8fea0dfb100425ff0dbc61b1140a2147370014
AEJ-Member <friedrich.seidenspinner@aejgmbh.com>
2021-06-14 09:38:17 +0200

    [ADD] Model PublicProject

---
+++ PublicProjekt/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name' : 'PubllicProject',
+    'version' : '0.1',
+    'summary': 'Project Access',
+    'description': """
+Ermöglicht den Zugang zu seinem Projekt als Public User
+    """,
+    'category': 'Projekt',
+    'website': 'https://www.deaejodoo.com/',
+    'depends' : ['base'],
+    'data': [
+
+    ],
+    'demo': [
+
+    ],
+    'qweb': [
+
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
