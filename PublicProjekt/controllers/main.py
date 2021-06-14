0e9b7bd5b4eee5c0fd5ac397d9e7ed7834737575
AEJ-Member <friedrich.seidenspinner@aejgmbh.com>
2021-06-14 19:12:54 +0200

    [ADD] add view and Controller Model PublicProject

--- PublicProjekt/controllers/main.py
+++ PublicProjekt/controllers/main.py
@@ -1,15 +1,15 @@
 # -*- coding: utf-8 -*-
 import odoo
-from odoo import fields, api, models
+from odoo import models, fields
 from odoo import http
 from odoo.http import request
-from odoo.addons.http_routing.controllers.main import WebClient, Routing
+
 
 class Gutachten(http.Controller):
 
     @http.route('/search_gutachten', type="http", auth="public", website=True)
-    def search_gutachten(self,**kw):
-        return http.request.render('',{})
+    def search_gutachten(self, **kw):
+        return http.request.render('PublicProject.search_gutachten', {})
 
 
 
