946e1b8200efa7dbb1a1b68d233ab001209528e4
AEJ-Member <friedrich.seidenspinner@aejgmbh.com>
2021-06-15 12:39:31 +0200

    [ADD] add correct view at PublicProject

--- PublicProjekt/controllers/main.py
+++ PublicProjekt/controllers/main.py
@@ -1,15 +1,14 @@
 # -*- coding: utf-8 -*-
-import odoo
-from odoo import models, fields
 from odoo import http
 from odoo.http import request
 
 
 class Gutachten(http.Controller):
 
-    @http.route('/search_gutachten', type="http", auth="public", website=True)
-    def search_gutachten(self, **kw):
-        return http.request.render('PublicProject.search_gutachten', {})
+    @http.route('/gutachten_webform1', type="http", auth="public", website=True)
+    def gutachten_webform1(self, **kw):
+       #gutachten = request.env['project.task'].sudo().search([])
+        return http.request.render("PublicProjekt.search_gutachten1", {})
 
 
 
